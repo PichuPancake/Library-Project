f25094d5c1d4c047911a4595a3de11513830d46e
PichuPancake <travis.li.us@gmail.com>
2016-03-03 00:14:55 -0500

    Update Book.java

--- Library Management System/Book.java
+++ Library Management System/Book.java
@@ -1,3 +1,27 @@
 public class Book{
+  private String isbn, name, author, category, status;
   
+  public Book(i, n, a, c, s){
+    isbn = i;
+    name = n;
+    author = a;
+    category = c;
+    status = s;
+  }
+  
+  public String getIsbn(){
+    return isbn;
+  }
+  public String getName(){
+    return name;
+  }
+  public String getAuthor(){
+    return author;
+  }
+  public String getCategory(){
+    return category;
+  }
+  public String getStatus(){
+    return status;
+  }
 }
