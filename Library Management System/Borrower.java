659af8261596bd856909f48eb3df77b99c2dad61
weixinTan <Tanw@bxscience.edu>
2016-03-03 00:37:57 -0500

    Update Borrower.java

--- Library Management System/Borrower.java
+++ Library Management System/Borrower.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
 public abstract class Borrower{
-  abstract void browse(String category);
-  abstract void avaliablity(int ISBN)
+  
+  public void browse(){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Please eneter the the category: ");
+    String c = sc.next();
+  }
+  public void avaliablity(){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Pleas enter the ISBN or the author of the book: ");
+    String c = sc.next();
+  }
+  
+  abstract void checkOut();
+  abstract void returnBook();
 }
