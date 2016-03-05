18db1ebb2de4c507a07e66280faf877ac6c20e5c
weixinTan <Tanw@bxscience.edu>
2016-03-05 13:52:22 -0500

    Update Teacher.java

--- Library Management System/Teacher.java
+++ Library Management System/Teacher.java
@@ -1,3 +1,22 @@
 public class Teacher extends Borrower{
+  public void checkOut(){
+    if(bookBorrowed ==4){
+      System.out.println("You have already borrowed maixmum amount of books ")
+    }
+    else{
+      
+    }
+    
+    
+  }
+  public void returnBook(){
+    if(bookBorrowed() ==0){
+      System.out.println("You did not borrow any book")
+    }
+    else{
+      
+    }
   
+  }
+
 }
