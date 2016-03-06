fe21ddff2542531a609465b4b6dfee02a20439f1
weixinTan <Tanw@bxscience.edu>
2016-03-06 00:18:40 -0500

    Update Teacher.java

--- Library Management System/Teacher.java
+++ Library Management System/Teacher.java
@@ -4,8 +4,21 @@ public class Teacher extends Borrower{
       System.out.println("You have already borrowed maixmum amount of books ")
     }
     else{
-      
-      bookBorrowed++;
+      Scanner sc = new Scanner(System.in);
+      System.out.print("To borrow a book please enter the ISBN or Author: ");
+      String c = sc.next();
+      int s =0//1 if book exist, 0 if book no
+      for(int i =0;i<books.size(),i++){
+        if(books.get(i).compare(c)){
+          books.remove(i);
+          printReceipt(books.get(i));
+          i=books.size();
+          bookBorrowed++;
+        }
+      }
+      if(s==0){
+        System.out.println("Book not found!");
+      }
     }
     
     
