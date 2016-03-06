0d958d7e517988521f4ecae2e2a778ff7115cae0
PichuPancake <travis.li.us@gmail.com>
2016-03-05 23:43:17 -0500

    Update LibraryTest.java

--- Library Management System/LibraryTest.java
+++ Library Management System/LibraryTest.java
@@ -1,6 +1,23 @@
+import java.util.*;
+
 public class LibraryTest{
   public static void main(String[] args){
-    Book b = new Book("917-2-16-124536-0","Test","Qu","Sci-fi","Not Borrowed");
-    
-  }
+    Scanner kb = new Scanner(System.in);
+    System.out.print("Librarian or borrower? ('librarian' or 'borrower'");
+    String user = kb.nextLine();
+    if(user.toUpperCase().equals("LIBRARIAN")){
+      Librarian librarian = new Librarian();
+      System.out.print("Adding or removing book? ('add' or 'remove')");
+      String action = kb.nextLine();
+      if(action.toUpperCase().equals("ADD"))
+        librarian.addBook();
+      if(action.toUpperCase().equals("REMOVE"))
+        librarian.removeBook();
+      else
+        System.out.println("Invalid input");
+    } if(user.toUpperCase().equals("BORROWER")){
+      System.out.print("Student or teacher? ('student' or 'teacher')");
+      String userBorrow = kb.nextLine();
+      if(userBorrow.toUpperCase().equals("STUDENT"))
+      
 }
