c1412eac67e3a56a9eb66eefe7d1e1755134c33c
weixinTan <Tanw@bxscience.edu>
2016-03-05 22:35:27 -0500

    Update Borrower.java

--- Library Management System/Borrower.java
+++ Library Management System/Borrower.java
@@ -2,18 +2,46 @@ import java.util.Scanner;
 import java.io.*;
 public abstract class Borrower{
   public int bookBorrowed;
+  public ArrayList<String> lines = new ArrayList<String>();
+  
   public void browse(){
     Scanner sc = new Scanner(System.in);
     System.out.print("Please eneter the the category: ");
     String c = sc.next();
     //then search the file and match enter category with already existed category and display it in a list
-    BufferedReader in = new BufferedReader (new FileReader("")
+    for(int i=0; i<lines.size();i++){
+      if(lines.get(i).contains(c)){
+        System.out.println(lines.get(i));
+      }
+    }
   }
   public void avaliablity(){
     Scanner sc = new Scanner(System.in);
     System.out.print("Pleas enter the ISBN or the author of the book: ");
     String c = sc.next();
     //then read and search file for the book and print not avaliable if borrowed else print avaliable.
+    int i=0;
+    int t=0;// 1 if book is avaliable, 0 is not
+    while(i<lines.size()){
+      if(line.get(i).contains(c)){
+        System.out.println("Book is avaliable to be borrow.");
+        System.out.println("Book: "+ lines.get(i));
+        c=lines.size();
+        t=1;
+      }
+      i++;
+    }
+    if(t=0){
+      System.out.println("Book is not avaliable to borrow, Sorry! ");
+    }
+  }
+  public void readFile(){
+    BufferedReader in = new BufferedReader(new FileReader(""));
+    String temp;
+    while ((temp = in.readLine()) !=null){
+      lines.add(temp);
+    }
+    in.close();
   }
   public int BookBorrowed(){
     return bookBorrowed;
