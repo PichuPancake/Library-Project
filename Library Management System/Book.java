a906e8a80525fba60f9c4bde3abfa6e7eb0ccf2a
PichuPancake <travis.li.us@gmail.com>
2016-03-06 15:12:21 -0500

    Update Book.java

--- Library Management System/Book.java
+++ Library Management System/Book.java
@@ -1,7 +1,7 @@
 public class Book{
   private String isbn, name, author, category, status;
   
-  public Book(i, n, a, c, s){
+  public Book(String i, String n, String a, String c, String s){
     isbn = i;
     name = n;
     author = a;
@@ -23,14 +23,5 @@ public class Book{
   }
   public String getStatus(){
     return status;
-  }
-  public boolean compare(String str){
-    return (isbn.equals(str) || name.equals(str) || author.equals(str)|| category.equals(str));
-  }
-  public boolean matches(Book otherBook){
-    return otherBook.isbn().equals(this.isbn) && otherBook.name().equals(this.name) && otherBook.author().equals(this.author)&&otherBook.category().equals(this.category);
-  }
-  public String toString(){
-    return isbn + "," + author + "," + name + "," category + "," + status;
   }
 }
