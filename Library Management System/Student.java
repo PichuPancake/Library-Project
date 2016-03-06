8a62d461bf40f5304f37de142acc32971478efe6
weixinTan <Tanw@bxscience.edu>
2016-03-05 23:43:09 -0500

    Update Student.java

--- Library Management System/Student.java
+++ Library Management System/Student.java
@@ -1,17 +1,24 @@
+import java.util.Scanner;
 public class Student extends Borrower{
-  private int bookOut, month, day;
-  public Student(int b, int m, int d){
-    bookOut = b;
-    month = m;
-    day = d;
-  }
   public void checkOut(){
     if(bookBorrowed ==2){
       System.out.println("You have already borrowed maixmum amount of books ")
     }
     else{
-      
-      bookBorrowed++;
+      Scanner sc = new Scanner(System.in);
+      System.out.print("To borrow a book please enter the ISBN or Author: ");
+      String c = sc.next();
+      int s =0//1 if book exist, 0 if book no
+      for(int i =0;i<books.size(),i++){
+        if(books.get(i).compare(c)){
+          printReceipt(books.get(i));
+          i=books.size();
+          bookBorrowed++;
+        }
+      }
+      if(s==0){
+        System.out.println("Book not found!");
+      }
     }
     
     
@@ -22,6 +29,7 @@ public class Student extends Borrower{
     }
     else{
       
+      books
       bookBorrowed--;
     }
   
