afac818dc23e65dc290351fd338bbd65aa58ab12
PichuPancake <travis.li.us@gmail.com>
2016-03-06 15:13:14 -0500

    Update LibraryTest.java

--- Library Management System/LibraryTest.java
+++ Library Management System/LibraryTest.java
@@ -13,11 +13,17 @@ public class LibraryTest{
         librarian.addBook();
       if(action.toUpperCase().equals("REMOVE"))
         librarian.removeBook();
-      else
-        System.out.println("Invalid input");
     } if(user.toUpperCase().equals("BORROWER")){
       System.out.print("Student or teacher? ('student' or 'teacher')");
       String userBorrow = kb.nextLine();
-      if(userBorrow.toUpperCase().equals("STUDENT"))
-      
+      if(userBorrow.toUpperCase().equals("STUDENT")){
+        Student student = new Student();
+        student.getAction();
+      } if(userBorrow.toUpperCase().equals("TEACHER")){
+        Teacher teacher = new Teacher();
+        teacher.getAction();
+      }
+    kb.close();     
+}
+}
 }
